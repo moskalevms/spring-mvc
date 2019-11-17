a8cbfba0915fc83176e06100b8a9842a4188a523
moskalevms <mis-van@yandex.ru>
2019-11-17 13:06:19 +0300

    ДЗ по третьему уроку

--- spring-jpa/src/main/java/moskalevms/persistence/CategoryRepository.java
+++ spring-jpa/src/main/java/moskalevms/persistence/CategoryRepository.java
@@ -3,7 +3,7 @@ package moskalevms.persistence;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import moskalevms. persistence.entity.Category;
+import moskalevms.persistence.entity.Category;
 
 import javax.persistence.EntityManager;
 import java.util.List;
